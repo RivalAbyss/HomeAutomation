2a51ee981e89f9537fab490c762dd1fc089fa8f1
RivalAbyss <149132226+RivalAbyss@users.noreply.github.com>
2023-12-11 21:33:12 +0530

    Update Code.ino

--- Code.ino
+++ Code.ino
@@ -10,10 +10,10 @@ void setup() {
   
   Serial.begin(9600);   // Initialize serial communication
   
-  digitalWrite(13, LOW);  // Turn off the bulb initially
+  digitalWrite(13, LOW);  // Turn off the bulb initially 
   digitalWrite(12, LOW);  // Turn off the fan initially
   
-  servo.attach(8);  // Attach the first servo motor to pin 5
+  servo.attach(8);  // Attach the servo motor to pin 5
 }
 
 void loop() {
@@ -32,73 +32,14 @@ void loop() {
     digitalWrite(12, HIGH);  // Turn off the fan if '4' is received
   }
 
-  if (digitalRead(7) ==1) {      
-    // Check if motion is detected
-    servo.write(90);          // Move the servo to a position (90 degrees) representing motion
-    delay(1000);                // Wait for servo movement (adjust delay as needed)
-    servo.write(0);           // Move the servo to another position (0 degrees) representing no motion
-    delay(1000);                // Wait for servo movement (adjust delay as needed)
-    servo.detach();
-  } 
-  else {
-    // servo.attach(5);
-    servo.write(90);          // Move the servo to a position (90 degrees) representing motion
-    delay(1000);                // Wait for servo movement (adjust delay as needed)
-    servo.write(0);           // Move the servo to another position (0 degrees) representing no motion
-    delay(1000);                // Wait for servo movement (adjust delay as needed)
-    servo.detach();
-  } 
-  // if (digitalRead(4) == HIGH) {
-  //   // Motion detected, move the servo
-  //   servo.write(90);  // Adjust the angle as needed
-  //   delay(1000);      // Wait for servo movement
-  //   servo.write(0);   // Reset servo position
-  //   delay(1000);
-  //   servo.detach();   // Detach the servo to save power (optional)
-  //   }
-
+  // Servo Motor and PIR Sensor
+  if(digitalRead(7)==HIGH){
+    servo.write(90);
+    delay(1000);
+  }
+  else{
+    servo.write(-200);
+    }
+  
   delay(100);  // Delay for stability
 }
-
-// void sm() {
-  // if (digitalRead(4) == HIGH) {      
-  //   // Check if motion is detected
-  //   servo.write(90);          // Move the servo to a position (90 degrees) representing motion
-  //   delay(1000);                // Wait for servo movement (adjust delay as needed)
-  //   servo.write(0);           // Move the servo to another position (0 degrees) representing no motion
-  //   delay(1000);                // Wait for servo movement (adjust delay as needed)
-  //   servo.detach();
-  // } else {
-  //   servo.attach(5);
-  // }
-  // PIR motion detection 
-//   int pirsensor = digitalRead(2);
-//   Serial.println(pirsensor);
-//   if (pirsensor == 1) 
-//   {
-//     digitalWrite(13, HIGH);  
-//     digitalWrite(4, HIGH);
-//   }
-//   else
-//   {
-//     digitalWrite(13,LOW);
-//     digitalWrite(4, LOW);
-//   }
-//   delay(10);
-// }
-  // if (digitalRead(8) == HIGH) {
-  //   // Motion detected, move the servo
-  //   servo.write(90);  // Adjust the angle as needed
-  //   delay(3000);      // Wait for servo movement
-  //   servo.write(-90);   // Reset servo position
-  //   delay(1000);
-  // int x = digitalRead(4);
-  // Serial.println(x);
-  // if(x==HIGH){
-  //   servo.write(180);
-  //   delay(3000);
-  // }
-  //  else{   
-  //   servo.write(-200);
-  // } 
-// }
